7d9a75ff0e79f864e411ee8c22e91b86d0148e3f
Sourav Kumar <souravbhadani94@gmail.com>
2015-05-17 10:38:23 +0530

    blog adding facility added

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,93 @@
+# Blog controller for creating blogs
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  # GET /blogs
+  # GET /blogs.json
+  def index
+    @blogs = Blog.all
+  end
+
+  # GET /blogs/1
+  # GET /blogs/1.json
+  def show
+  end
+
+  # GET /blogs/new
+  def new
+    @blog = Blog.new
+  end
+
+  # GET /blogs/1/edit
+  def edit
+  end
+
+  # POST /blogs
+  # POST /blogs.json
+  def create
+    @blog = Blog.new(blog_params)
+
+    respond_to do |format|
+      if @blog.save
+        format.html do
+          redirect_to @blog,
+          notice: 'Blog was successfully created.'
+        end
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        format.json do
+          render json: @blog.errors,
+                 status: :unprocessable_entity
+        end  
+      end
+    end
+  end
+
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html do
+          redirect_to @blog,
+          notice: 'Blog was successfully updated.' 
+        end  
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json do
+          render json: @blog.errors,
+          status: :unprocessable_entity 
+        end         
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @blog.destroy
+    respond_to do |format|
+      format.html do
+        redirect_to blogs_url,
+        notice: 'Blog was successfully destroyed.'
+      end  
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    # Use callbacks to share common setup
+    # or constraints between actions.
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
+  def blog_params
+    params.require(:blog).permit(:title, :desc, :published_on)
+  end
+end
---
+++ app/views/blogs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @blog, :id, :title, :desc, :published_on, :created_at, :updated_at
